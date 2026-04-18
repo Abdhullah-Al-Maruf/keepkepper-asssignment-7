90517faa85a2936a3df05843b9cc7ef38c0f5e7a
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-04-18 10:24:10 +0600

    added filterlogic

--- src/pages/TimeLine/Timeline.jsx
+++ src/pages/TimeLine/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { HistoryContext } from '../../context/TimelineContext';
 import { GiEmptyMetalBucket } from 'react-icons/gi';
 import Emptytimeline from '../../component/ui/Emptytimeline';
@@ -6,48 +6,83 @@ import Emptytimeline from '../../component/ui/Emptytimeline';
 const Timeline = () => {
     const { personData } = useContext(HistoryContext);
 
+    //  filter state
+    const [filter, setFilter] = useState("All");
 
+    // filter logic
+    const filteredData =
+        filter === "All"
+            ? personData
+            : personData.filter(person => person.type === filter);
 
     return (
         <>
-            <h2>TimeLine</h2>
+            <div className='w-[90%] md:container mx-auto'>
+                <div>
+                    <h2 className='text-2xl font-bold mt-5 mb-5'>TimeLine</h2>
+                    {/* filter ui */}
+                    <button
+                        className="btn mb-4"
+                        popoverTarget="popover-filter"
+                        style={{ anchorName: "--anchor-filter" }}
+                    >
+                        Filtered By :{filter}
+                    </button>
 
-            {
-                personData.length === 0 ?<Emptytimeline/> : <div>
-                    {
-                        personData.map((person, ind) => <div className='container mx-auto flex  gap-5 shadow-md border border-gray-200 mt-5 mb-5 p-5'
-                            key={ind}>
-                            <div className='text-3xl'>
-                                {
-                                    `  ${person.type === 'Call' ? "📞" :
-                                        person.type === "Text" ? "💬" : "📽️"
-                                    }`
-                                }
-                            </div>
-                            <div>
-                                <h2 className='text-gray-500'>
-                                    <span className='font-bold text-black'>{person.type}</span> with {person.friendData.name}
-                                </h2>
-                                <h2>
-                                    {new Date().toLocaleString("en-US", {
-                                        day: "numeric",
-                                        month: "long",
-                                        year: "numeric",
-                                        hour: "2-digit",
-                                        minute: "2-digit"
-                                    })}
-                                </h2>
-                            </div>
-
-                        </div>)
-                    }
+                    <ul
+                        className="dropdown menu w-52 rounded-box bg-base-100 shadow-sm"
+                        popover="auto"
+                        id="popover-filter"
+                        style={{ positionAnchor: "--anchor-filter" }}
+                    >
+                        <li><a onClick={() => setFilter("All")}>All</a></li>
+                        <li><a onClick={() => setFilter("Call")}>Call</a></li>
+                        <li><a onClick={() => setFilter("Text")}>Text</a></li>
+                        <li><a onClick={() => setFilter("Video")}>Video</a></li>
+                    </ul>
                 </div>
 
-            }
+                {/* Timeline content */}
+                {
+                    personData.length === 0 ? <Emptytimeline /> : <div>
+                        {
+                            filteredData.map((person, ind) => (
+                                <div
+                                    className='container mx-auto flex gap-5 shadow-md border border-gray-200 mt-5 mb-5 p-5'
+                                    key={ind}
+                                >
+                                    <div className='text-3xl'>
+                                        {
+                                            `  ${person.type === 'Call' ? "📞" :
+                                                person.type === "Text" ? "💬" : "📽️"
+                                            }`
+                                        }
+                                    </div>
 
+                                    <div>
+                                        <h2 className='text-gray-500'>
+                                            <span className='font-bold text-black'>{person.type}</span> with {person.friendData.name}
+                                        </h2>
+
+                                        <h2>
+                                            {new Date().toLocaleString("en-US", {
+                                                day: "numeric",
+                                                month: "long",
+                                                year: "numeric",
+                                                hour: "2-digit",
+                                                minute: "2-digit"
+                                            })}
+                                        </h2>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                }
+
+            </div>
 
         </>
-
     );
 };
 
